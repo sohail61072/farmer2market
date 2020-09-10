425513fb75ab6ab4d986a0852035d4eb33635272
Shehla61297 <Shehla61297@GB-LD1LPW10G013.mastek.com>
2020-09-10 12:54:57 +0100

    add tests created for all daos.

--- src/test/java/com/mastek/farmertomarket/FarmertomarketApplicationTests.java
+++ src/test/java/com/mastek/farmertomarket/FarmertomarketApplicationTests.java
@@ -1,13 +1,118 @@
 package com.mastek.farmertomarket;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.mastek.farmertomarket.dao.BasketJPADAO;
+import com.mastek.farmertomarket.dao.CheckoutJPADAO;
+import com.mastek.farmertomarket.dao.CustomerJPADAO;
+import com.mastek.farmertomarket.dao.FarmerJPADAO;
+import com.mastek.farmertomarket.dao.ItemJPADAO;
+import com.mastek.farmertomarket.dao.ProductJPADAO;
+import com.mastek.farmertomarket.dao.TransactionJPADAO;
+import com.mastek.farmertomarket.entities.Checkout;
+import com.mastek.farmertomarket.entities.Customer;
+import com.mastek.farmertomarket.entities.FarmType;
+import com.mastek.farmertomarket.entities.Farmer;
+import com.mastek.farmertomarket.entities.Item;
+import com.mastek.farmertomarket.entities.Product;
+import com.mastek.farmertomarket.entities.ProductType;
+import com.mastek.farmertomarket.entities.Transaction;
+
+//Initialize the JUnit Test with Spring Boot Application Environment
+//Spring Boot Test: used to test Spring APplicationContext 
+//with all the test cases idenitfied
 @SpringBootTest
 class FarmertomarketApplicationTests {
-
+	
+	@Autowired
+	BasketJPADAO baskDAO;
+	
+	@Autowired
+	CheckoutJPADAO checkDAO;
+	
+	@Autowired
+	CustomerJPADAO custDAO;
+	
+	@Autowired
+	FarmerJPADAO farmDAO;
+	
+	@Autowired
+	ItemJPADAO itemDAO;
+	
+	@Autowired
+	ProductJPADAO prodDAO;
+	
+	@Autowired
+	TransactionJPADAO tranDAO;
+	
 	@Test
-	void contextLoads() {
+	void testCheckoutDAOAdd() {
+		Checkout check = new Checkout();
+		check.setDeliveryDate("21/09/2020");
+		check.setTotalCost(22.29);
+		
+		check = checkDAO.save(check);
 	}
-
-}
+	
+	@Test
+	void testCustomerDAOAdd() {
+		Customer cust = new Customer();
+		cust.setCustomerForename("Mary");
+		cust.setCustomerSurname("Smith");
+		cust.setCustomerDOB("18/08/1994");
+		cust.setCustomerUsername("m.smith");
+		cust.setCustomerAddress("22 Mangolia Crescent, Leeds, LS11PA");
+		cust.setCustomerPassword("123456");
+		
+		cust = custDAO.save(cust);
+		
+	}
+	
+	@Test
+	void testFarmerDAOAdd() {
+		Farmer farm = new Farmer();
+		farm.setFarmerForename("Harry");
+		farm.setFarmerSurname("Prince");
+		farm.setFarmLocation("Ormskirk");
+		farm.setFarmName("Berry Knot Farm");
+		farm.setFarmType(FarmType.MIXED);
+		farm.setFarmerUsername("BerryKnotFarm");
+		farm.setFarmerPassword("BKnot123");
+		
+		farm = farmDAO.save(farm);
+	}
+	@Test
+	void testItemDAOAdd() {
+		Item it = new Item();
+		it.setItemName("Potatoes");
+		it.setItemPrice(0.59);
+		it.setItemQuantity(10.00);
+		
+		it = itemDAO.save(it);
+		
+	}
+	@Test
+	void testProductDAOAdd() {
+		Product prod = new Product();
+		prod.setProductName("Potatoes");
+		prod.setProductType(ProductType.VEGETABLES);
+		prod.setProductDescription("White potatoes");
+		prod.setProductPrice(0.59);
+		prod.setProductQuantity(100);
+		prod.setProductWeightKG(5.00);
+		
+		prod = prodDAO.save(prod);
+	}
+	
+	@Test
+	void testTransactionDAOAdd() {
+		Transaction tran = new Transaction();
+		tran.setTransactionDate("22/09/2020");
+		tran.setTransactionAmount(10.00);
+		
+		tran = tranDAO.save(tran);
+	}
+	}
+	
