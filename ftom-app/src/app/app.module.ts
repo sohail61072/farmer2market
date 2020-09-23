59b6b3e337c62ef464ff39508a0fdc700a578f4c
Shehla Jahangir <Shehla61297@Mastek.Com>
2020-09-23 11:23:53 +0100

    app created

---
+++ ftom-app/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BasketComponent } from './basket/basket.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CustomerComponent } from './customer/customer.component';
+import { FarmerComponent } from './farmer/farmer.component';
+import { ItemComponent } from './item/item.component';
+import { ProductComponent } from './product/product.component';
+import { TransactionComponent } from './transaction/transaction.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasketComponent,
+    CheckoutComponent,
+    CustomerComponent,
+    FarmerComponent,
+    ItemComponent,
+    ProductComponent,
+    TransactionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
